7253c2783c5b2628d4970ebf3e4897a8cda08942
kishna081 <kishna081@yahoo.co.jp>
2018-12-03 21:34:01 +0900

    1203java

---
+++ Iphone/src/Android.java
@@ -0,0 +1,13 @@
+
+public class Android extends Mp3player{
+	public static void main(String[] args){
+	SmartPhone Android = new SmartPhone();
+	Android.play();
+	Android.stop();
+	Android.back();
+	Android.call();
+	Android.mail();
+	Android.photo();
+	Android.internet();
+}
+}
